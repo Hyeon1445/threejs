51e8c2fced8c5a33c8af2d82e9618cd6e6284150
hyeon1445 <lily1445@gmail.com>
2023-10-08 12:35:43 +0900

    feat: add mesh with box geometry and basic material

--- 02-basic/src/main.js
+++ 02-basic/src/main.js
@@ -1,16 +1,29 @@
 import * as THREE from "three";
 
 const canvas = document.querySelector("#three-canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true }); // antialias: 계단현상 방지&성능저하
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Scene
 const scene = new THREE.Scene();
+
+// Camera
 const camera = new THREE.PerspectiveCamera( // 원근카메라
-  75, // 시야갹
+  75, // 시야갹(field of view)
   window.innerWidth / window.innerHeight, // aspect ratio
   0.1, // near
   1000 // far
 );
 camera.position.z = 5;
+camera.position.y = 2;
+camera.position.x = 1;
 scene.add(camera);
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 그리기
 renderer.render(scene, camera);
