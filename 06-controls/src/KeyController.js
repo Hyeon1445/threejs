1aaec9f81417dd243a928801c8de879e0e98dda3
hyeon1445 <lily1445@gmail.com>
2023-10-08 22:12:19 +0900

    feat: implement key controller with pointer lock controls

---
+++ 06-controls/src/KeyController.js
@@ -0,0 +1,11 @@
+export class KeyController {
+  constructor() {
+    this.keys = [];
+    window.addEventListener("keydown", (e) => {
+      this.keys[e.code] = true;
+    });
+    window.addEventListener("keyup", (e) => {
+      delete this.keys[e.code];
+    });
+  }
+}
