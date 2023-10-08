c64a0bd6a548b37a22a6aa890308d93a2ee230c9
hyeon1445 <lily1445@gmail.com>
2023-10-08 12:26:54 +0900

    feat: add camera

--- 02-basic/src/main.js
+++ 02-basic/src/main.js
@@ -3,3 +3,14 @@ import * as THREE from "three";
 const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( // 원근카메라
+  75, // 시야갹
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // near
+  1000 // far
+);
+camera.position.z = 5;
+scene.add(camera);
+renderer.render(scene, camera);
